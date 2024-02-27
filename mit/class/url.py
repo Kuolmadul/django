d7f20c4f20aaa802820797e1cee67676bcfd94c2
pk-focus <orinamitchel10@gmail.com>
2024-02-27 07:46:13 +0300

    h

--- mit/class/url.py
+++ mit/class/url.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path
 
-urlpatterns = {
-
+urlpatterns = [
     path('', views.home),
     path('about/', views.about),
     path('contact/', views.contact),
     path('location/', views.location)
+]
 
-}
+
+
